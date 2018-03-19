9e4f9ff0900f784a4331ad20828e4aa6f889ed51
PederHA <peder.andresen@gmail.com>
2018-03-19 17:28:16 +0100

    Refactored wowprogress cog code

--- cogs/wowprogress_cog.py
+++ cogs/wowprogress_cog.py
@@ -29,7 +29,7 @@ class WPCog:
 
     @commands.command(name="wowprogress",
                       aliases=["wp", "wowp"],
-                      description="Searches Wowprogress for a given guild name and returns top result ")
+                      description="Searches WoWprogress for a given guild name, and returns top result.")
     #@ExtModule.reaction_respond
     async def wowprogress(self, ctx: commands.Context, *args: str):
         try:
@@ -41,49 +41,44 @@ class WPCog:
             soup = bs.BeautifulSoup(sauce, 'html.parser')
 
             # Finds the top search result and fetches url to guild page
-            topr = soup.find(
+            top_result = soup.find(
                 "a", {"class": lambda L: L and L.startswith("guild")})
 
             # Sends error message to the channel in case a guild cannot be found
-            if topr == None:
+            if top_result == None:
                 return await ctx.send("Couldn't find guild!")
 
             # Top result is cast to string, then appended to wowprogress URL
-            # to create the complete URL to the guild page
-            topr = str(topr.get("href"))
-            appendresult = "https://wowprogress.com" + topr
+            # to create the full website URL to the guild page
+            top_result = str(top_result.get("href"))
+            guild_wp_url = "https://wowprogress.com" + top_result
 
             # Scrapes the resulting URL from the top result
-            sauce = urllib.request.urlopen(appendresult).read()
+            sauce = urllib.request.urlopen(guild_wp_url).read()
             soup = bs.BeautifulSoup(sauce, "html.parser")
 
             # Raids Per Week
-            wprw = soup.find("div", class_="raids_week")
+            raids_per_week = soup.find("div", class_="raids_week")
 
             # Manage exception for guilds with unknown raid days
 
             """
             DUNNO WHAT TO DO WITH THIS YET
-            #unknown = wprw.find("nobr")
+            #unknown = raids_per_week.find("nobr")
             #print(unknown.string)
-            #print(wprw.string)
-            #if wprw.string == None:
+            #print(raids_per_week.string)
+            #if raids_per_week.string == None:
                 #rpwn = "unknown"
             
-            #elif wprw.string != None:
+            #elif raids_per_week.string != None:
             """
 
-            rpw = str(wprw.string)
-            rpw = rpw.split(": ", 1)[1]
+            raids_per_week = str(raids_per_week.string).split(": ", 1)[1]
 
-            # elif unknown.string == None:
-            # print(rpw)
 
             # Guild name
-            gname = soup.find("title")
-            gname = str(gname.string)
-            gname = gname.partition("@")[0]
-            gname = gname.split("Guild ", 1)[1]
+            guild_name = soup.find("title")
+            guild_name = str(guild_name.string).partition("@")[0].split("Guild ", 1)[1]
 
             # Progression
             progress = soup.find("span", class_="innerLink ratingProgress")
@@ -91,17 +86,17 @@ class WPCog:
             progress = str(progress.string)
 
             # World Rank
-            wrank = soup.find(
+            world_rank = soup.find(
                 "span", {"class": lambda L: L and L.startswith("rank")})
+            world_rank = world_rank.string
 
             # Calculate execution time
             exetime = (time.time()-start_time)
             exetime = "{0:.1f}".format(exetime)
 
-            # TODO: Use .format() instead.
-            #      Also, not sure what I was doing with regards to using the bs4 string method vs explicitly casting to string back then.
-            return await ctx.send(appendresult + "\n```\n\n" + "Guild Name: " + gname + "\nProgress: " + progress + "\nWorld Rank: "
-                                  + wrank.string + "\n" + "Raids Per Week: " + rpw + "\n```" + "\n" + "👳🏾" + "Command executed in " + exetime + " seconds")
+            # TODO: Markdown formatting (bold).    
+            await ctx.send(guild_wp_url + ("\n\n```markdown\n" + "Guild Name: {}\nProgress: {}\nWorld Rank: {}"
+                            "\nRaids Per Week: {}\n```\n👳🏾Command executed in {} seconds").format(guild_name, progress, world_rank, raids_per_week, exetime))        
         except Exception:
             # Lazy solution, but this will be the correct error message to return in 99% of cases.
-            return await ctx.send("Guild is inactive.")
+            await ctx.send("Guild is inactive.")
