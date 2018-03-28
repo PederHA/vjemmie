26eb4d0b45887b25b85041ba2787107d7882fd59
PederHA <peder.andresen@gmail.com>
2018-03-28 19:45:32 +0200

    Sorted imports & prepping for control via serial

--- pfmbot.py
+++ pfmbot.py
@@ -1,22 +1,25 @@
-import secrets
-from discord.ext.commands import Bot
-from discord.ext import commands
-import discord
-import bs4 as bs
 import asyncio
-from cogs.sound_cog import SoundboardCog
-from cogs.admin_cog import AdminCog
-from cogs.user_cog import UserCog
-from cogs.pubg_cog import PUBGCog
-from cogs.youtube_cog import YTCog
-from cogs.wowprogress_cog import WPCog
-from cogs.pfm_memes_cog import PFMCog
-from cogs.reddit_cog import RedditCog
-from cogs.fun_cog import FunCog
-from discord import opus
-from ext_module import ExtModule
-from events_module import EventsModule
+
+import secrets
 from secrets import PFMSecrets
+
+import discord
+from discord import opus
+from discord.ext import commands
+from discord.ext.commands import Bot
+
+from cogs.admin_cog import AdminCog
+from cogs.fun_cog import FunCog
+from cogs.pfm_memes_cog import PFMCog
+from cogs.pubg_cog import PUBGCog
+from cogs.reddit_cog import RedditCog
+from cogs.sound_cog import SoundboardCog
+from cogs.user_cog import UserCog
+from cogs.wowprogress_cog import WPCog
+from cogs.youtube_cog import YTCog
+from cogs.serialsound_cog import SerialSoundboardCog
+from events_module import EventsModule
+from ext_module import ExtModule
 
 bot = Bot(command_prefix="!", description="Meme bot", pm_help=False)
 sound_folder = 'sounds'
@@ -31,14 +34,18 @@ bot.add_cog(WPCog(bot=bot, log_channel_id=log_channel_id))
 bot.add_cog(PFMCog(bot=bot, log_channel_id=log_channel_id))
 bot.add_cog(RedditCog(bot=bot, log_channel_id=log_channel_id))
 bot.add_cog(FunCog(bot=bot, log_channel_id=log_channel_id))
+bot.add_cog(SerialSoundboardCog(bot=bot, folder=sound_folder, log_channel_id=log_channel_id))
 
 @bot.async_event
 def on_ready():
     print("Client logged in")
 
+
 # Move out of main?
 @bot.listen()
 async def on_message(message):
+    if EventsModule.is_travis(message):
+        pass
     if EventsModule.is_rad(message):
         if any(word in message.content.lower() for word in secrets.RAD_WORDS):
             await message.add_reaction(':PedoRad:237754662361628672')
