82942d879be6cae3738d213cdf6a5035e3c79d87
PederHA <peder.andresen@gmail.com>
2018-04-05 20:10:28 +0200

    Added some more checks

--- events_module.py
+++ events_module.py
@@ -2,13 +2,28 @@ from discord.ext import commands
 import discord
 
 class EventsModule:
-    # is_member using dict instead?
+    # Consolidate all these methods to avoid clutter?
     @staticmethod
     def is_travis(message):
         if message.author.id == 103890994440728576:
             return True
     
     @staticmethod
+    def is_hoob(message):
+        if message.author.id == 133908820815642624:
+            return True
+    
+    @staticmethod
+    def is_huya(message):
+        if message.author.id == 133697550623571969:
+            return True
+    
+    @staticmethod
     def is_rad(message):
         if message.author.id == 133875264995328000:
-            return True    
+            return True
+    
+    @staticmethod
+    def contains_rad(content):
+        if "rad" in content:
+            return True
