f8708d13f891acfe59da80f789f8d975eceb75c6
PederHA <peder.andresen@gmail.com>
2018-11-26 19:11:10 +0100

    Improved argument parsing for !play. Fixed gTTS

--- cogs/sound_cog.py
+++ cogs/sound_cog.py
@@ -5,8 +5,7 @@ import os
 import asyncio
 import random
 from discord import opus
-from gtts import gTTS
-import pickle
+import gtts
 from pathlib import Path
 import youtube_dl
 
@@ -14,9 +13,7 @@ import youtube_dl
 class SoundboardCog:
     """Cog for the soundboard feature"""
 
-    # see play command changes
-
-    def __init__(self, bot: commands.Bot, folder=None, log_channel_id: int=None, tag_dict: dict={}):
+    def __init__(self, bot: commands.Bot, folder=None, log_channel_id: int=None, tag_dict: dict={}) -> None:
         """The constructor for the SoundboardCog class, it assigns the important variables used by the commands below
         Args:
             bot: The bot the Cog will be added to (commands.Bot)
@@ -26,32 +23,28 @@ class SoundboardCog:
         self.folder = folder
         self.bot = bot
         self.log_channel_id = log_channel_id
-        self.send_log = None
 
 
     @property
-    def sound_list(self):
-        """This function returns a list with all the mp3-file names in the given folder
-        Args:
-            folder: The folder with the mp3 files (String)
+    def sound_list(self) -> list:
+        """
+        Returns list of files in the sound folder provided on object instantiation.
+        
         Returns:
-            sound_list: The list with file names, all lowercase and without the .mp3 (list)
-        This function raises an Exception, if the folder was empty
-            """
+            sound_list: list of file names, all lowercase and without the .mp3 file extension.
+        
+        Raises Exception if the folder contains no .mp3 files.
+        """
         sound_list = sorted([i[:-4].lower()
                              for i in os.listdir(self.folder) if '.mp3' in i])
-        
-        # Pickle sound_list 
-        #with open("soundlist.pkl", "wb") as f:
-            #pickle.dump(sound_list, f)
 
         if not sound_list:
             raise Exception("No mp3 files in the given folder")
         return sound_list
 
-    async def on_ready(self):
+    async def on_ready(self) -> None:
         self.send_log = ExtModule.get_send_log(self)
-        opus.load_opus('libopus')  # the opus library
+        opus.load_opus('libopus')
 
     @commands.command(name='play',
                       description=' tags/name || Plays the sound with the first name found in the arguments.'
@@ -60,7 +53,7 @@ class SoundboardCog:
                                   ' If no tags are found or no sound with all the given tags exists'
                                   ' the sound is chosen randomly.'
                                   ' Requires you to be in a voice channel!')
-    async def play(self, ctx: commands.Context, *args):
+    async def play(self, ctx: commands.Context, *args) -> None:
         """This command plays the first sound name found in args, if one exists.
         If none exists, all args will be interpreted as tags. The command will create the cut, of all
         valid tag parameters and play a random file from that.
@@ -76,11 +69,11 @@ class SoundboardCog:
             await ctx.send(content='To use this command you have to be connected to a voice channel!')
             raise discord.DiscordException
 
-        # Find sound file in sound folder
-        for arg in args:  # check if one is a name
-            if arg.lower() in self.sound_list:
-                sound_name = arg.lower()
-                break 
+
+
+        arg = " ".join(args).lower()
+        if arg in self.sound_list:
+            sound_name = arg
         else:
             if len(args)>0:
                 await ctx.send(f"Could not find sound with name {args[0]}")
@@ -95,14 +88,14 @@ class SoundboardCog:
                            ' Please try again later or stop me with the stop command!')
             raise discord.DiscordException
         vc.play(discord.FFmpegPCMAudio(self.folder + '/' + sound_name + '.mp3'),
-                after=lambda e: SoundboardCog.disconnector(vc, self.bot))
-        await self.send_log('Playing: ' + sound_name)
+                after=lambda e: self.disconnector(vc))
+        #await self.send_log('Playing: ' + sound_name)
 
     @commands.command(name='stop',
                       aliases=['halt'],
                       description='The bot will stop playing a sound and leave the current voice channel.'
                                   'Requires you to be in the same voice channel as the bot!')
-    async def stop(self, ctx: commands.Context):
+    async def stop(self, ctx: commands.Context) -> None:
         """This function stops the bot playing a sound and makes it leave the current voice channel.
          Args:
              ctx: The context of the command, which is mandatory in rewrite (commands.Context)
@@ -113,7 +106,7 @@ class SoundboardCog:
 
     @commands.command(name='soundlist',
                       aliases=['sounds'], description='Prints a list of all sounds on the soundboard.')
-    async def soundlist(self, ctx: commands.Context):
+    async def soundlist(self, ctx: commands.Context) -> None:
         """This function prints a list of all the sounds on the Soundboard to the channel/user where it was requested.
         Args:
             ctx: The context of the command, which is mandatory in rewrite (commands.Context)
@@ -130,21 +123,20 @@ class SoundboardCog:
         await ctx.send(f"```{sound_string}```")
 
 
-    @staticmethod
-    def disconnector(voice_client: discord.VoiceClient, bot: commands.Bot):
+    def disconnector(self, voice_client: discord.VoiceClient) -> None:
         """This function is passed as the after parameter of FFmpegPCMAudio() as it does not take coroutines.
         Args:
             voice_client: The voice client that will be disconnected (discord.VoiceClient)
             bot: The bot that will terminate the voice client, which will be this very bot
             """
         coro = voice_client.disconnect()
-        fut = asyncio.run_coroutine_threadsafe(coro, bot.loop)
+        fut = asyncio.run_coroutine_threadsafe(coro, self.bot.loop)
         try:
             fut.result()
         except asyncio.CancelledError:
             pass
 
-    async def name_reaction(self, name, message):
+    async def name_reaction(self, name, message) -> None:
         if name == "mw2nuke":
             await message.add_reaction('\U0001F4A3')
         # Looks in spellbook for Pick Lock
@@ -153,12 +145,12 @@ class SoundboardCog:
 
     @commands.command(name="texttospeech",
                       aliases=["tts", "text-to-speech"])
-    async def texttospeech(self, ctx: commands.Context, *args):
-        valid_langs = gTTS.LANGUAGES.keys()
+    async def texttospeech(self, ctx: commands.Context, *args) -> None:
+        valid_langs = gtts.lang.tts_langs()
         if len(args) == 3:
             text, language, sound_name = args
-            if language in valid_langs:
-                tts = gTTS(text=text, lang=language)
+            if language in valid_langs.keys():
+                tts = gtts.gTTS(text=text, lang=language)
                 file_path = f"sounds/{sound_name}.mp3"
                 # Check if a file with identical name already exists
                 if Path(file_path).exists():
@@ -182,8 +174,8 @@ class SoundboardCog:
             if len(args) >= 1:
                 if args[0] in ["languages", "lang", "options"]:
                     output = '```Available languages:\n\n'
-                    for lang in gTTS.LANGUAGES:
-                        output += f"{gTTS.LANGUAGES[lang]}: {lang}\n"
+                    for language_long, language_short in valid_langs.items():
+                        output += f"{language_long}: {language_short}\n"
                     output += "```"
                     await ctx.send(output)
                 else:
@@ -194,21 +186,21 @@ class SoundboardCog:
                                 "\nType `!tts lang` for more information about available languages.")
 
     @commands.command(name="ytdl")
-    async def ytdl(self, ctx: commands.Context, *args):
+    async def ytdl(self, ctx: commands.Context, *args) -> None:
         if len(args)>0:
             url = args[0]
         ydl_opts = {
-                'outtmpl': 'sounds/%(id)s.%(ext)s',
-                'format': 'bestaudio/best',
+                'outtmpl': 'sounds/%(title)s.%(ext)s',
+                'format': 'bestaudio',
                 'postprocessors': [{
                     'key': 'FFmpegExtractAudio',
-                    'preferredcodec': 'mp3',
-                    'preferredquality': '192'}]
+                    'preferredcodec': 'mp3'}]
                     }
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             result = ydl.extract_info(url=url, download=True, extra_info=ydl_opts)
             res = ydl.prepare_filename(result)
             path, _ = res.split(".")
+            print(path)
             _, _file = path.split("\\")
 
         try:
