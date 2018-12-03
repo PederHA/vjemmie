f622a474d85f72c84559cc1f62fc8ff99770f459
PederHA <peder.andresen@gmail.com>
2018-12-03 17:52:55 +0100

    defined __all__ for wildcard import

---
+++ cogs/__init__.py
@@ -0,0 +1,20 @@
+from cogs.admin_cog import AdminCog
+from cogs.fun_cog import FunCog
+from cogs.pfm_memes_cog import PFMCog
+from cogs.pubg_cog import PUBGCog
+from cogs.reddit_cog import RedditCog
+from cogs.sound_cog import SoundboardCog
+from cogs.user_cog import UserCog
+from cogs.wowprogress_cog import WPCog
+from cogs.youtube_cog import YTCog
+#fromcogs..serialsound_cog import SerialSoundboardCog
+from cogs.soundbomb_cog import SoundBombCog
+from cogs.testing_cog import TestingCog
+from cogs.frying_cog import FryingCog
+from cogs.weather_cog import WeatherCog
+from cogs.cod_cog import CodCog
+from cogs.war3_cog import War3Cog
+
+__all__ = ["AdminCog", "FunCog", "PFMCog", "PUBGCog", "RedditCog", "SoundboardCog", "UserCog",
+           "WPCog", "YTCog", "SoundBombCog", "TestingCog", "FryingCog", "WeatherCog", "CodCog",
+           "War3Cog"]
