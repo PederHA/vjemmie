17f279971f69e36cd4e06901e4da767fecda8980
PederHA <peder.andresen@gmail.com>
2018-12-17 16:56:35 +0100

    Refactor !result. Still terrible.

--- cogs/war3_cog.py
+++ cogs/war3_cog.py
@@ -2,8 +2,9 @@ import asyncio
 import math
 import sqlite3
 import traceback
-from itertools import combinations
+from itertools import combinations, chain
 from typing import Tuple
+from collections import namedtuple
 
 import discord
 import trueskill
@@ -17,13 +18,13 @@ class War3Cog(BaseCog):
     def __init__(self, bot: commands.Bot, log_channel_id: int, replays_folder: str) -> None:
         super().__init__(bot, log_channel_id)
         self.replays_folder = replays_folder
-        self.db = DatabaseHandler("db/rating.db")
+        self.db = DatabaseHandler("db/testrating.db")
         self.base_rating = 25
         trueskill.setup(sigma=2.5)
 
     
     @commands.command(name="add")
-    async def add_user(self, ctx: commands.Context, user_id: int, realname: str, game="legiontd") -> None:
+    async def add_user(self, ctx: commands.Context, user_id: int, realname: str=None, game="legiontd") -> None:
         for member in self.bot.get_all_members():
             # This is the only way I found to obtain an arbitrary user's nickname
             # self.bot.get_user() returns a User obj, which does not have a nick attribute
@@ -34,27 +35,27 @@ class War3Cog(BaseCog):
             await ctx.send(msg)
     
     @commands.command(name="leaderboard")
-    async def leaderboard(self, ctx: commands.Context, game="legiontd", alias: bool=False, post_game: bool=False) -> None:
+    async def leaderboard(self, ctx: commands.Context, game="legiontd", alias: bool=False, *, post_game: bool=False) -> None:
         players = self.db.get_players(game, alias=alias)
         output = "```\nDGVGK Legion TD Rankings:\n\n"
         for player in players:
             name, rating, wins, losses = player
-            name = name.capitalize()
-            rating = round(rating*40)
-            if len(name) > 10:
-                name = name[:10]
-            # Align rows. Kinda hacky, looking for better solution.
-            name_spc = " " * (10 - len(name))
-            rating_spc = " " * (10 - len(str(rating)))
-            wins_spc = " " * (6 - len(str(wins)))
-            output += f"{name}{name_spc}\tRating: {rating}{rating_spc}Wins: {wins}{wins_spc}Losses: {losses}\n"
+            if not (wins == 0 and losses == 0):
+                name = name.capitalize()
+                rating = round(rating*40)
+                if len(name) > 10:
+                    name = name[:10]
+                # Align rows. Kinda hacky, looking for better solution.
+                name_spc = " " * (10 - len(name))
+                rating_spc = " " * (10 - len(str(rating)))
+                wins_spc = " " * (6 - len(str(wins)))
+                output += f"{name}{name_spc}\tRating: {rating}{rating_spc}Wins: {wins}{wins_spc}Losses: {losses}\n"
+        output += "```"
+        if post_game:
+            deletion_timer = 60.0
         else:
-            output += "```"
-            if post_game:
-                deletion_timer = 60.0
-            else:
-                deletion_timer = None
-            await ctx.send(output, delete_after=deletion_timer)
+            deletion_timer = None
+        await ctx.send(output, delete_after=deletion_timer)
         
     
     @commands.command(name="result")
@@ -74,12 +75,12 @@ class War3Cog(BaseCog):
         
         # List from string of names separated by spaces. Remove duplicates.
         winners = list(set(winners.lower().split(" ")))
-        winners.sort()  
+        #winners.sort()  
         # Copy of winners that will not be modified
         winners_names = list(winners)    
         if losers is not None:
             losers = list(set(losers.lower().split(" ")))
-            losers.sort()
+            #losers.sort()
             losers_names = list(losers)
         else:
             # Detect losing team from voice channel if `losers` is None
@@ -98,92 +99,80 @@ class War3Cog(BaseCog):
                     await ctx.send("Aborting rating update.")
                     raise Exception
         
+        # Could use sets for this as well, but this is pretty clear
+        for name in winners:
+            if name in losers:
+                await ctx.send(f"{name} cannot be on both teams.")
+                raise Exception("!add: Duplicate name on winning and losing team")
+        
         # Get players from db as list of tuples        
         players = self.db.get_players(game, alias=alias)
         _players = [player for player, _, _, _ in players]
-        # Temporary lists that will replace winners/losers after names are parsed
+        # Lists holding Player tuples that will replace winners/losers after names are parsed
         _winners = []
         _losers = []        
 
-        def filter_participants(participant: str) -> None:
-            """
-            OMEGAGIGA ugly function for matching players in db with
-            players in `winners` and `losers`.
-
-            I don't like scattering specialized methods around in my Cogs. 
-            Since this function will only ever be used by `result()`, 
-            it is defined here.
-            """
-
-            nonlocal winners
-            nonlocal _winners
-            nonlocal losers
-            nonlocal _losers
-            nonlocal players
-            nonlocal _players
-            if participant in _players and not (participant in winners and participant in losers):
-                for player, rating, wins, losses in players:
-                    for winner in winners:
-                        if player == winner:
-                            _winners.append((player, rating, wins, losses))
-                            winners.remove(player)
-                            break
-                    for loser in losers:    
-                        if loser == player:
-                            _losers.append((player, rating, wins, losses))
-                            losers.remove(player)
-                            break
-
-        for participant in winners+losers:
-            if participant in _players and not (participant in winners and participant in losers):
-                filter_participants(participant)
-            elif participant not in _players or (participant in winners and participant in losers):
-                abort = "abort"
-                cmd = self.bot.get_command("add")
-                await ctx.send(f"Could not find player {participant}.\n" 
-                               f"Add {participant} to database by typing user's User ID.\n"
-                               f"Type **{abort}** to abort")               
-                try:
-                    msg = await self.bot.wait_for("message", check=pred, timeout=15.0)
-                # Raise exception if 15 seconds pass without reply from user
-                except asyncio.TimeoutError:
-                    await ctx.send("No reply from user. Aborting.")
-                    raise Exception
-                
-                # If user responds, check message content
-                if msg.content.lower() == abort:
-                    await ctx.send("Aborting. Rating will not be updated.")
-                    raise Exception("Rating update aborted by user.")    
-                else:
-                    user_id, *alias = msg.content.split(" ")
-                    if alias != []:
-                        alias = alias[0]
-                    else:
-                        alias = None
+        # Loop runs until all players are parsed
+        while len(_winners+_losers) != len(winners+losers):
+            for winner in winners:
+                for player in players:
+                    if player.name == winner:
+                        _winners.append(player)
+                        break    
+            for loser in losers:
+                for player in players:    
+                    if player.name == loser:
+                        _losers.append(player)
+                        break                 
+            # Prompt to add player to database if player is not found in db
+            unknown_players = [name for name in winners+losers if name not in _players]
+            if unknown_players:
+                for name in unknown_players:
+                    abort = "abort"
+                    cmd = self.bot.get_command("add")
+                    await ctx.send(f"Could not find player {name}.\n" 
+                                f"Add {name} to database by typing: `<User ID> (optional: <Real Name>)`.\n"
+                                f"Type **{abort}** to abort")               
                     try:
-                        user_id = int(user_id)
-                    except:
-                        await ctx.send("User ID must be an integer. Aborting.")
-                        raise Exception(f"User provided non-int user_id: {user_id}")
+                        msg = await self.bot.wait_for("message", check=pred, timeout=15.0)
+                    # Raise exception if 15 seconds pass without reply from user
+                    except asyncio.TimeoutError:
+                        await ctx.send("No reply from user. Aborting.")
+                        raise Exception
+                    
+                    # If user responds, check message content
+                    if msg.content.lower() == abort:
+                        await ctx.send("Aborting. Rating will not be updated.")
+                        raise Exception("Rating update aborted by user.")    
                     else:
-                        await ctx.invoke(cmd, user_id, alias, game)
-                        players = self.db.get_players(game, alias=alias)
-                        _players = [player for player, _, _, _ in players]
-                        filter_participants(participant)
+                        user_id, *realname = msg.content.split(" ")
+                        if realname != []:
+                            realname = realname[0]
+                        else:
+                            realname = None
+                        try:
+                            user_id = int(user_id)
+                        except:
+                            await ctx.send("User ID must be an integer. Aborting.")
+                            raise Exception(f"User provided non-int user_id: {user_id}")
+                        else:
+                            await ctx.invoke(cmd, user_id, realname, game)
+                            players = self.db.get_players(game, alias=False)
+                            _players = [player for player, _, _, _ in players]
+                            #filter_participants(participant)
+        else:
+            # After loop, replace lists of names with list of player tuples
+            winners = _winners
+            losers = _losers
 
-        winners = _winners
-        losers = _losers
-        ts_winners = []
-        ts_losers = []
-        
         # Create trueskill.Rating objects for players
+        ts_winners = []
         for winner in winners:
-            name, rating, wins, losses = winner
-            _winner = trueskill.Rating(mu=rating)
+            _winner = trueskill.Rating(mu=winner.rating)
             ts_winners.append(_winner)
+        ts_losers = []
         for loser in losers:
-            name, rating, wins, losses = loser
-            _loser = trueskill.Rating(mu=rating)
+            _loser = trueskill.Rating(mu=loser.rating)
             ts_losers.append(_loser)            
 
         # Calculate new ratings
@@ -199,27 +188,33 @@ class War3Cog(BaseCog):
             else:
                 winners_new, losers_new = trueskill.rate([tuple(ts_winners), tuple(ts_losers)], 
                             weights=[tuple([quality for i in winners]), tuple([quality for x in losers])])
-        
         # Update ratings in DB and generate Discord message 
-        if not preview: 
+        if not preview:
+            def update_rating(names: list, players_pre: list, players_post: list, db, *, win: bool, game: str) -> str:
+                _msg = ""
+                for idx, player in enumerate(players_post):
+                    db.update_rating(names[idx], player, win=True, game=game)
+                    pre_rating = round(players_pre[idx].mu*40)
+                    post_rating = round(player.mu*40)
+                    rating_change = round(post_rating - pre_rating)
+                    if rating_change > 0:
+                        symbol = "+"
+                    else:
+                        symbol = ""
+                    _msg += f"{names[idx].capitalize()}: {pre_rating} -> {post_rating} ({symbol}{rating_change})\n"
+                return _msg
             msg = "```Rating change:\n\n"
-            for idx, winner in enumerate(winners_new):
-                self.db.update_rating(winners_names[idx], winner, win=True, game=game)
-                rating_gain = round((winner.mu - ts_winners[idx].mu)*40)
-                msg += f"{winners[idx][0].capitalize()}: +{rating_gain} rating.\n"
-            for idx, loser in enumerate(losers_new):
-                self.db.update_rating(losers_names[idx], loser, win=False, game=game)
-                rating_loss = round((ts_losers[idx].mu - loser.mu)*40)
-                msg += f"{losers[idx][0].capitalize()}: -{rating_loss} rating.\n"   
+            msg += update_rating(winners_names, ts_winners, winners_new, self.db, win=True, game=game)
+            msg += update_rating(losers_names, ts_losers, losers_new, self.db, win=False, game=game) 
             msg += "```"
 
             # Post rating change message
             await ctx.send(msg, delete_after=60.0)
             cmd = self.bot.get_command("leaderboard")
             # Post overall leaderboard
-            await ctx.invoke(cmd, game)
+            await ctx.invoke(cmd, game, post_game=True)
         
-        if preview:
+        elif preview:
             # Rating change is the same for all players in a team
             rating_gain = round((winners_new[0].mu - ts_winners[0].mu)*40) # Index is irrelevant
             rating_loss = round((ts_losers[0].mu - losers_new[0].mu)*40)
@@ -231,6 +226,8 @@ class War3Cog(BaseCog):
         Distributes players to 2 teams based on their rating.
         """
 
+        MIN_PLAYERS = 3
+
         help_str = """Usage: !teams "player1 player2 player3 player4" (opt: <game>)
                       If command is called without arguments, pulls players from 
                       message author's voice channel."""
@@ -238,15 +235,19 @@ class War3Cog(BaseCog):
         if players is None or players == "c":
             # Get players from author's voice channel if no argument to param `players` is given
             players = await self.get_users_in_voice(ctx)
-            if players is None:
+            if players is None or players is []:
                 await ctx.send("You are not connected to a voice channel")
-                raise Exception
-            elif len(players) == 1:
-                await ctx.send("At least 2 players have to be present in the voice channel.")
+                raise discord.DiscordException("User is not connected to a voice channel")
+            elif len(players) < MIN_PLAYERS:
+                await ctx.send(f"At least {MIN_PLAYERS} players have to be present in the voice channel.")
                 raise Exception
         else:
             # Create list from string of names separated by spaces
             players = players.split(" ")
+
+        if len(players) < MIN_PLAYERS:
+            await ctx.send("A minimum of 3 players is required.")
+            raise ValueError("Need at least 3 players to make teams")
         
         try:
             db_players = self.db.get_players(game)
@@ -254,26 +255,28 @@ class War3Cog(BaseCog):
             err = traceback.format_exc()
             await self.send_log(err)
             await ctx.send(f"Could not find player stats for game **{game}**.")
-            raise Exception
+            raise
         
         players_ratings = []
+        #Player = namedtuple("player", "name rating wins losses")
         for db_player in db_players:
             name, rating, wins, losses = db_player
+            #_player = Player(name, rating, wins, losses)
             for player in players:
                 if name == player:
                     players_ratings.append(db_player)
                     break
-        
+
         n_players = len(players_ratings)
         _combs = combinations(players_ratings, r=(math.ceil((n_players)/2)))
         # Sort list of player combinations by sum of player ratings from low->high
         combs = sorted(_combs, key=lambda x: sum([rating for _, rating, _, _ in x]))
         # Generate team1
-        if len(players_ratings) % 2 == 0:
+        if n_players % 2 == 0:
             middle = float(len(combs))/2
             if middle % 2 != 0:
                 # If middle index is not an integer, test both [middle + .5] and [middle - .5]
-
+                
                 # Calculate average rating for first potential team 1
                 _t1_1 = combs[int(middle + .5)] 
                 _t1_1_rating = sum([rating for _, rating, _, _ in _t1_1])
@@ -307,7 +310,9 @@ class War3Cog(BaseCog):
             
             else:              
                 team1 = combs[int(middle)]   
-        
+        elif n_players == 1:
+            # This shouldn't happen 
+            raise Exception
         else:
             # If teams are uneven, stack the biggest team with the worst players
             # Lazy implementation for now
