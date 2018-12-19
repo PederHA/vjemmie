91cab8a25bab247836a9b419116d9bfd9fb28471
PederHA <peder.andresen@gmail.com>
2018-12-19 18:17:42 +0100

    Fix a lot of shitty code, disabled !drop because cba rn

--- cogs/pubg_cog.py
+++ cogs/pubg_cog.py
@@ -12,110 +12,111 @@ class PUBGCog(BaseCog):
     """PUBG Bot Commands
     """
     
-    @commands.command(
-        name="drop", aliases=["roulette", "plane"], description="u fucking wot"
-    )
-    async def drop(self, ctx: commands.Context, *args):
-        """
-        ``!drop``command that chooses random drop location on a given PUBG map.
-        ----
-        
-        Args: tuple containing user arguments provided after typing !drop.
-        """
-
-        MIRAMAR_LOCATIONS_ALL = [
-            "El Pozo",
-            "Pecado",
-            "San Martín",
-            "Hacienda del Patrón",
-            "Campo Militar",
-            "Los Leones",
-            "Monte Nuevo",
-            "El Azahar",
-            "Cruz del Valle",
-            "Tierra Bronca",
-            "Torre Ahumada",
-            "Impala",
-            "La Cobrería",
-        ]
-
-        MIRAMAR_LOCATIONS_HOT = MIRAMAR_LOCATIONS_ALL[:6]
-
-        ERANGEL_LOCATIONS_ALL = [
-            "South George",
-            "North George",
-            "Yasnaya",
-            "School",
-            "Pochinki",
-            "Mylta",
-            "Mylta Power",
-            "Military Base",
-            "Novorepnoye",
-            "Lipovka",
-            "Prison",
-            "Shelter",
-            "Primorsk",
-            "Gatka",
-            "Zharki",
-        ]
-
-        ERANGEL_LOCATIONS_HOT = ERANGEL_LOCATIONS_ALL[:9]
-
-        VALID_MAPS = ["Erangel", "Miramar", "Sanhok [NYI!]"]
-        invalid_args_msg = 'Invalid arguments. Syntax: "!drop <map> (hot)"'
-        invalid_map = False
-        no_map = False
-
-        # Only valid_args if an argument is given.
-        if args != ():
-
-            pubgmap = args[0].lower()
-            pubgmap = pubgmap[0:].capitalize()
-
-            # Check if arg is a valid pubg map
-            if pubgmap in VALID_MAPS:
-                if pubgmap == "Miramar":
-                    if len(args) > 1:
-                        # If the second argument is 'hot', pick location from hot list.
-                        if args[1] == "hot":
-                            droplocation = random.choice(MIRAMAR_LOCATIONS_HOT)
-                        else:
-                            await ctx.send(invalid_args_msg)
-                    else:
-                        droplocation = random.choice(MIRAMAR_LOCATIONS_ALL)
-
-                elif pubgmap == "Erangel":
-                    if len(args) > 1:
-                        if args[1] == "hot":
-                            droplocation = random.choice(ERANGEL_LOCATIONS_HOT)
-                        else:
-                            await ctx.send(invalid_args_msg)
-                    else:
-                        droplocation = random.choice(ERANGEL_LOCATIONS_ALL)
-
-                await ctx.send("Drop location: " + droplocation)
-
-            else:
-                invalid_map = True
-
-        else:
-            no_map = True
-        
-        if no_map or invalid_map:
-            if no_map:
-                message = "No map specified. Type "
-            if invalid_map:
-                message = "Invalid map specified. Type "
-            
-            n = 0
-            for pubgmap in VALID_MAPS:
-                n += 1
-                message+= f"`!drop {pubgmap} (hot)`"
-                if n < len(VALID_MAPS):
-                    message += " or "
-            else:
-                message += "."
-            await ctx.send(message)
+    #@commands.command(
+    #    name="drop", aliases=["roulette", "plane"], description="u fucking wot"
+    #)
+    #async def drop(self, ctx: commands.Context, map_:str=None, opt: str=None):
+    #    """
+    #    ``!drop``command that chooses random drop location on a given PUBG map.
+    #    ----
+    #    
+    #    Args: tuple containing user arguments provided after typing !drop.
+    #    """
+#
+    #    MIRAMAR_LOCATIONS_ALL = [
+    #        "El Pozo",
+    #        "Pecado",
+    #        "San Martín",
+    #        "Hacienda del Patrón",
+    #        "Campo Militar",
+    #        "Los Leones",
+    #        "Monte Nuevo",
+    #        "El Azahar",
+    #        "Cruz del Valle",
+    #        "Tierra Bronca",
+    #        "Torre Ahumada",
+    #        "Impala",
+    #        "La Cobrería",
+    #    ]
+#
+    #    MIRAMAR_LOCATIONS_HOT = MIRAMAR_LOCATIONS_ALL[:6]
+#
+    #    ERANGEL_LOCATIONS_ALL = [
+    #        "South George",
+    #        "North George",
+    #        "Yasnaya",
+    #        "School",
+    #        "Pochinki",
+    #        "Mylta",
+    #        "Mylta Power",
+    #        "Military Base",
+    #        "Novorepnoye",
+    #        "Lipovka",
+    #        "Prison",
+    #        "Shelter",
+    #        "Primorsk",
+    #        "Gatka",
+    #        "Zharki",
+    #    ]
+#
+    #    ERANGEL_LOCATIONS_HOT = ERANGEL_LOCATIONS_ALL[:9]
+#
+    #    VALID_MAPS = ["erangel", "miramar", "sanhok [NYI!]"]
+    #    HOT = ["hot"]
+    #    invalid_args_msg = 'Invalid arguments. Syntax: "!drop <map> (hot)"'
+    #    invalid_map = False
+    #    no_map = False
+    #    pubgmap = map_.lower()
+    #    
+    #    if pubgmap not in VALID_MAPS:
+    #        await ctx.send("Invalid")
+    #        raise Exception
+#
+    #    # Only valid_args if an argument is given.
+    #    if map_ is not None:
+#
+    #        
+    #        if opt.lower() == "hot":
+    #            hot = True
+    #        else:
+    #            hot = False
+#
+    #        # Check if arg is a valid pubg map
+    #        if pubgmap in VALID_MAPS:
+    #            if pubgmap == VALID_MAPS[1]
+    #                if hot:
+    #                    droplocation = random.choice(MIRAMAR_LOCATIONS_HOT)
+    #                else:
+    #                    droplocation = random.choice(MIRAMAR_LOCATIONS_ALL)
+    #            elif pubgmap == VALID_MAPS[0]
+    #                if hot:
+    #                    droplocation = random.choice(ERANGEL_LOCATIONS_HOT)
+    #                else:
+    #                    droplocation = random.choice(ERANGEL_LOCATIONS_ALL)
+#
+    #            await ctx.send("Drop location: " + droplocation)
+#
+    #        else:
+    #            invalid_map = True
+#
+    #    else:
+    #        no_map = True
+    #    
+    #    if no_map or invalid_map:
+    #        if no_map:
+    #            message = "No map specified. Type "
+    #        if invalid_map:
+    #            message = "Invalid map specified. Type "
+    #        
+    #        n = 0
+    #        for pubgmap in VALID_MAPS:
+    #            n += 1
+    #            message+= f"`!drop {pubgmap} (hot)`"
+    #            if n < len(VALID_MAPS):
+    #                message += " or "
+    #        else:
+    #            message += "."
+    #        await ctx.send(message)
 
 
     # Start of Crate command
@@ -142,16 +143,12 @@ class PUBGCog(BaseCog):
         if args == ():
             squad = default_squad
         elif args[0].isdigit():
-            number_of_players = int(args[0])
-            if 1 < number_of_players <= 4:
-                int_list = list(range(number_of_players + 1))
-                int_list = int_list[1:]
-                squad_list = []
-                for n in int_list:
-                    n = str(n)
-                    squad_list.append(n)
+            n_players = int(args[0])
+            if 1 < n_players <= 4:
+                int_list = list(range(1, n_players))
+                squad_list = [str(n) for n in int_list]
                 squad = squad_list
-            elif number_of_players > 4:
+            elif n_players > 4:
                 bot_msg = "Can't roll crate for more than 4 players."
             else:
                 bot_msg = "The specified range is too low. 2-4 players are needed."
@@ -166,25 +163,24 @@ class PUBGCog(BaseCog):
         else:
             squad = args
         
-        if bot_msg != "":
+        if bot_msg:
             enough_players = False # If any `bot_msg` is specified, command will exit.
-
-        if not enough_players:
             await ctx.send(bot_msg)
+            raise Exception(f"Exception in !crate: {bot_msg}")
 
-        else:
-            # Create a list from the `*args`` tuple to make it mutable.
-            squad = list(squad)
 
-            if EventsModule.contains_rad(squad):
-                await ctx.message.add_reaction(":8xscope:417396529452810241")
+        # Create a list from the `*args`` tuple to make it mutable.
+        squad = list(squad)
 
-            # Determines size of squad and distributes guns accordingly.
-            # Returns size of squad and gun list containing n=squadsize lists.
-            gunsplit, armorsplit = await self.roll_guns(squad)
-            output = await self.generate_crate_text(squad, gunsplit, armorsplit)
+        if EventsModule.contains_rad(squad):
+            await ctx.message.add_reaction(":8xscope:417396529452810241")
 
-            await ctx.send(output)
+        # Determines size of squad and distributes guns accordingly.
+        # Returns size of squad and gun list containing n=squadsize lists.
+        gunsplit, armorsplit = await self.roll_guns(squad)
+        output = await self.generate_crate_text(squad, gunsplit, armorsplit)
+
+        await ctx.send(output)
 
     async def roll_guns(self, squad):
         _CRATEGUNS_ALL = [
@@ -222,32 +218,22 @@ class PUBGCog(BaseCog):
 
         return gunsplit, armorsplit
 
-    async def generate_crate_text(self, squad, gunsplit, armorsplit):
-        squadsize = len(squad)
-        
-
-
-        # Generate discord bot output
-        output = "```"        
+    async def generate_crate_text(self, squad: list, gunsplit: list, armorsplit: list) -> str:
+        """
+        Create output message for !crate command.
+        """
+               
         if squad[0].isdigit(): 
             # Sort squad numerically
             squad.sort() 
-        
-        for n in range(squadsize):    
-            # Create string object from list index
-            squad_member = squad[n] 
-            
-            if squad_member.islower():
-                squad_member = squad_member.capitalize()
-
-            gun = str(gunsplit[n])[1:-1].replace("'", "")
-            equipment = str(armorsplit[n])[1:-1].replace("'", "")
-            text_line = ""
-            text_line = f"{squad_member}: " f"{gun} " f"{equipment}\n"
-            output += text_line
-        
-        output += "```"
-        return output
+        msg = "```"
+        for idx, player in enumerate(squad):
+            name_spc = " "*(12-len(player))
+            gun = " ".join(gunsplit[idx])
+            equipment = " ".join(armorsplit[idx])
+            msg += f"{player.capitalize()}:{name_spc} {gun} {equipment}\n"
+        msg += "```"
+        return msg
 
     async def get_squad_from_channel(self, ctx):
         """
