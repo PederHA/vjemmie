c9891faf9e1d142f096da7c07852d9d282676afe
PederHA <peder.andresen@gmail.com>
2019-02-13 19:50:57 +0100

    Add !add_alias, !remove_alias. Improve time/content filtering commands

--- cogs/reddit_cog.py
+++ cogs/reddit_cog.py
@@ -13,6 +13,8 @@ import datetime
 from functools import partialmethod,  partial
 import pickle
 from collections import namedtuple
+from itertools import cycle
+
 
 reddit = praw.Reddit(
     client_id=secrets.REDDIT_ID,
@@ -38,9 +40,12 @@ class RedditCog(BaseCog):
 
     def __init__(self, bot: commands.Bot, log_channel_id: int=None) -> None:
         super().__init__(bot, log_channel_id)
-        self.subs = self.load_subs() # Load list of subs from disk 
+        self.subs = self.load_subs() # Load list of subs from disk
         for sub in self.subs:
             self._add_sub(sub)
+        # Generators for changing reddit sorting.
+        self.time_cycle = cycle(self.TIME_FILTERS) # Command: !rtime
+        self.sorting_cycle = cycle(self.SORTING_FILTERS) # Command: !rsort
 
     def load_subs(self) -> list:
         with open("db/subreddits.pkl", "rb") as f:
@@ -114,34 +119,92 @@ class RedditCog(BaseCog):
         await ctx.send(out)
 
     @commands.command(name="change_reddit_time", aliases=["rtime", "reddit_time"])
-    async def change_time_filtering(self, ctx: commands.Context, time) -> None:
-        time = await self.check_time(ctx,time)
-        self.DEFAULT_TIME = time
-        await ctx.send(f"Reddit time filtering set to **{time}**.")
-    
-    @commands.command(name="toggle_hot", aliases=["hot"])
-    async def toggle_hot(self, ctx: commands.Context) -> None:
-        if self.DEFAULT_SORTING != "hot": # At this point I have given up not hardcoding these things
-            self.DEFAULT_SORTING = "hot"
+    async def change_time_filtering(self, ctx: commands.Context, opt: str=None) -> None:
+        out_msg = None # Hacky, but w/e
+        if opt:
+            if opt in ["s", "status", "show"]:
+                out_msg = f"Reddit time filtering is currently set to **{self.DEFAULT_TIME}**"
+            else:
+                try:
+                    time = await self.check_time(ctx,opt)
+                except:
+                    await ctx.send(f"Invalid argument {time}")
+                else:
+                    # Match up time filter cycle and current time
+                    _next_time = next(self.time_cycle)
+                    while time != _next_time:
+                        _next_time = next(self.time_cycle)
         else:
-            self.DEFAULT_SORTING = "top"
-        await ctx.send(f"Reddit content sorting set to **{self.DEFAULT_SORTING}**")
+            time = next(self.time_cycle) # Pump generator if no opt argument
+        if not out_msg:
+            self.DEFAULT_TIME = time
+            out_msg = f"Reddit time filtering set to **{time}**."
+        await ctx.send(out_msg)
+    
+    @commands.command(name="rsort", aliases=["hot", "top"])
+    async def change_content_sorting(self, ctx: commands.Context, status: str=None) -> None:
+        """
+        Toggles between "hot" and "top" content filtering on Reddit.
 
-    @commands.command(name="change_sub_command")
-    async def change_sub_command(self, ctx: commands.Context, subreddit: str, command: str) -> None:
-        if not command.isnumeric():
-            command = command.lower()
-            for sub_full, sub_short in self.subs:
-                if sub_full == subreddit:
-                    self.subs.remove((sub_full, sub_short))
-                    self.subs.append((subreddit, command))
+        ----
+        
+        Aliases `!hot` and `!top` allow for manual selection of content filtering, while
+        main command `!rsort` toggles between "hot" & "top"
+        
+        Args:
+            status (str, optional): Defaults to None. Optional user argument for 
+            displaying current content sorting. 
+
+        """
+        if not status:
+            if ctx.invoked_with == "hot":
+                self.DEFAULT_SORTING = await self.check_sorting(ctx, "hot") # Too complicated? Prevents errors, though
+            elif ctx.invoked_with == "top":
+                self.DEFAULT_SORTING = await self.check_sorting(ctx, "top")
+            else:
+                sort = next(self.sorting_cycle)
+                if sort == self.DEFAULT_SORTING: # Avoid setting same value as current if !top or !hot has been used
+                    sort = next(self.sorting_cycle)
+                self.DEFAULT_SORTING = sort
+            msg = f"Reddit content sorting set to {self.DEFAULT_SORTING}"
+        elif status in ["status", "show", "s"]:
+            msg = f"Reddit content sorting is currently set to {self.DEFAULT_SORTING}"
+        else:
+            msg = "Usage:\n\n+ !rsort switches between hot/top\n+ !hot & !top for manual selection"
+        await ctx.send(await self.make_codeblock(msg, "diff"))
+    
+    @commands.command(name="add_alias")
+    async def change_sub_command(self, ctx: commands.Context, subreddit: str, alias: str) -> None:
+        if not alias.isnumeric():
+            alias = alias.lower()
+            for idx, subreddit_cmd in enumerate(self.subs):
+                if subreddit == subreddit_cmd.subreddit:
+                    self.subs.pop(idx)
+                    subreddit_cmd.aliases.append(alias)
+                    self.subs.append(subreddit_cmd)
                     self.dump_subs()
-                    await ctx.send(f"Command for subreddit **r/{subreddit}** changed to **!{command}**.\n"
-                                    "Changes take effect on next restart.")
+                    await ctx.send(f"Added alias **!{alias}** for subreddit **r/{subreddit}**")
+                    #await ctx.send(f"Command for subreddit **r/{subreddit}** changed to **!{command}**.\n"
+                    #                "Changes take effect on next restart.")
                     break
         else:
-            await ctx.send("Invalid command name. Can only contain letters a-z.")
-        
+            await ctx.send("Invalid alias name. Can only contain letters a-z.")
+    
+    @commands.command(name="remove_alias")
+    async def remove_alias(self, ctx: commands.Context, subreddit: str, alias: str) -> None:
+        for idx, subreddit_cmd in enumerate(self.subs):
+            if subreddit == subreddit_cmd.subreddit:
+                try:
+                    subreddit_cmd.aliases.remove(alias)
+                except:
+                    await ctx.send(f"No such alias **!{alias}** for subreddit **r/{subreddit}**")
+                else:
+                    self.subs.pop(idx)
+                    self.subs.append(subreddit_cmd)
+                    self.dump_subs()
+                    await ctx.send(f"removed alias **!{alias}** for subreddit **r/{subreddit}**")
+                break        
+
     @commands.command(name="reddit")
     async def reddit(self, ctx: commands.Context, subreddit: str, sorting: str=None, time: str=None) -> None:
         try:
@@ -154,7 +217,7 @@ class RedditCog(BaseCog):
             filter_ = default_filter
         else:
             if filter_ not in valid_filters:
-                await self._send_error(ctx, f"{filtering_type} filters", valid_filters)
+                await self._send_error(ctx, f"{filtering_type} filters", valid_filters) # Sends message and raises exception
         if filter_:
             return filter_
 
