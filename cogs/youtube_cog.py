c00199b9df1087ee72a84c37ca48f485b7a3d379
PederHA <peder.andresen@gmail.com>
2019-03-04 16:44:55 +0100

    Retired !youtube until I cba fixing it

--- cogs/youtube_cog.py
+++ cogs/youtube_cog.py
@@ -14,11 +14,15 @@ class YouTubeCog(BaseCog):
     @commands.command(name='youtube',
                         aliases=['yt', 'utube'],
                         description='YouTube search that returns top result for search query.')                       
-    async def youtube(self, ctx: commands.Context, *args: str):
-        start_time = time.time()
+    async def youtube(self, ctx: commands.Context, *search_query: str):
+        """Retired for now"""
+        raise discord.DiscordException("Command is unavailable right now due to changes to YouTube's search results.")
         
+        start_time = time.time()
+        if not search_query:
+            raise discord.DiscordException("A search query is required!")
         url = ("https://www.youtube.com/results?{}".format(
-                urlencode({'search_query': ' '.join(args)}) 
+                urlencode({'search_query': ' '.join(search_query)}) 
                 ))
         sauce = urllib.request.urlopen(url).read()
         soup = bs.BeautifulSoup(sauce,"html.parser")
