473a8c0f04fb07fe14a12d97bac23fc608d9163a
PederHA <peder.andresen@gmail.com>
2019-03-04 14:38:28 +0100

    Add !delete_messages command

--- cogs/admin_cog.py
+++ cogs/admin_cog.py
@@ -4,8 +4,9 @@ from ext_module import ExtModule
 from ext_module import PmForbidden
 from cogs.admin_utils import load_blacklist, save_blacklist, is_admin
 from cogs.base_cog import BaseCog
-from typing import Union
+from typing import Union, Optional
 import asyncio
+from datetime import datetime, timedelta
 
 
 class AdminCog(BaseCog):
@@ -155,6 +156,7 @@ class AdminCog(BaseCog):
             await ctx.invoke(show_cmd, "blacklist")
 
     @commands.command(name="show")
+    @is_admin()
     async def show_xlist(self, ctx: commands.Context, list_name: str) -> None:
         list_name = list_name.lower()
         out_list = None
@@ -213,3 +215,22 @@ class AdminCog(BaseCog):
         cmd = [x for x in self.bot.commands if x.name == "remove_sub"][0]
         breakpoint()
 
+    @commands.command(name="delete_messages", aliases=["dlt"])
+    @is_admin()
+    async def delete_messages(self, 
+                              ctx: commands.Context, 
+                              member: Optional[str]=None,
+                              content: Optional[str]=None) -> None:
+        after = datetime.now() - timedelta(hours=2)
+        # Any 1 char member name is interpreted as None
+        member = await commands.MemberConverter().convert(ctx, member) if len(member)!=1 else None
+        n = 0
+        async for msg in ctx.message.channel.history(limit=250, after=after):
+            if content and content in msg.content and msg.content != ctx.message.content:
+                await msg.delete()
+                n += 1
+            if member and member.name == msg.author.name:
+                await msg.delete()
+                n += 1
+        s = "s" if n>1 else ""
+        await ctx.send(f"```\nDeleted {n} message{s}```")
