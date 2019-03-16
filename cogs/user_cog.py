b8b18b90fd8e48476f306b607134d91ad586535a
PederHA <peder.andresen@gmail.com>
2019-03-16 14:01:27 +0100

    Improved simple/advanced handling I think? idk

--- cogs/user_cog.py
+++ cogs/user_cog.py
@@ -11,21 +11,22 @@ class UserCog(BaseCog):
         self.bot.remove_command('help')
 
     @commands.command(name="help", aliases=["Help", "hlep", "?", "pls"])
-    async def send_help(self, ctx: commands.Context, cog_name: str=None, simple: str="y"):
+    async def send_help(self, ctx: commands.Context, cog_name: str=None, simple: str="simple"):
         """Sends information about a specific cog's commands"""
         if simple in ["n", "-", "advanced"]:
-            simple = False
+            simple = "advanced"
         if not cog_name:
             categories = "\n".join(cog.cog_name
                                    for cog in list(self.bot.cogs.values())
                                    if cog.cog_name not in self.IGNORE_HELP)
             embed = await self.get_embed(ctx, fields=[self.EmbedField("Categories", categories)])
             await ctx.send(embed=embed)
-            await ctx.send("Type `!help <category>` or `!<Category>`")
+            await ctx.send("Type `!help <category> (advanced)` or `!<Category> (advanced)`")
         else:
-            cog_name = cog_name
+            cog_name = cog_name.lower()
             for cog in self.bot.cogs.values():
-                if cog.cog_name == cog_name or cog_name.capitalize() == cog.cog_name and cog_name not in self.IGNORE_HELP:
+                if cog_name == cog.cog_name.lower() and cog_name not in self.IGNORE_HELP:
+
                     await cog._get_cog_commands(ctx, simple)
                     break
             else:
