4cf85f7abaf9e1ded92be93cf22a6e7060896011
PederHA <peder.andresen@gmail.com>
2019-04-01 15:35:30 +0200

    Remove checks

--- pfmbot.py
+++ pfmbot.py
@@ -1,19 +1,14 @@
 import asyncio
-import sys
-
 import secrets
+import sys
 from secrets import PFMSecrets
 
 import discord
 from discord.ext import commands
 from discord.ext.commands import Bot
 
-
 from cogs import *
-from utils.config import GENERAL_DB_PATH
-from cogs.db_cog import DatabaseHandler
 from ext.checks import load_blacklist
-
 
 # Bot setup
 bot = Bot(command_prefix="!", description="Meme bot", pm_help=False)
@@ -22,17 +17,6 @@ bot = Bot(command_prefix="!", description="Meme bot", pm_help=False)
 for k, v in dict(locals()).items():
     if k.endswith("Cog"):
         bot.add_cog(v(bot=bot))
-
-#@bot.check_once
-#def check_commands(ctx):
-#    return ctx.message.author.id not in load_blacklist()
-
-@bot.listen()
-async def on_message(message):
-    if PFMSecrets.offensive_word(message) and message.author.id != bot.user.id:
-        db = DatabaseHandler(GENERAL_DB_PATH)
-        db.log_gaming_moment(message)
-        await message.add_reaction(':cmonpfm:427842995748995082')      
 
 dev = False if len(sys.argv)==1 else True
 token = secrets.BOT_TOKEN if not dev else secrets.DEV_BOT_TOKEN
