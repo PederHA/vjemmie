8eaac7dab6333467885fdf71a820b11c3fd8e221
PederHA <peder.andresen@gmail.com>
2019-04-02 12:24:29 +0200

    Add dynamic generation of __all__ contents

--- cogs/__init__.py
+++ cogs/__init__.py
@@ -1,3 +1,12 @@
+"""
+Add/remove cogs in this module.
+
+NOTE
+----
+Only subclasses of discord.ext.commands.Cog are added to __all__!
+"""
+from discord.ext import commands
+
 from cogs.admin_cog import AdminCog
 from cogs.fun_cog import FunCog
 from cogs.pfm_memes_cog import PFMCog
@@ -5,7 +14,6 @@ from cogs.pubg_cog import PUBGCog
 from cogs.reddit_cog import RedditCog
 from cogs.user_cog import UserCog
 from cogs.wowprogress_cog import WoWProgressCog
-from cogs.youtube_cog import YouTubeCog
 from cogs.testing_cog import TestingCog
 from cogs.frying_cog import FryingCog
 from cogs.weather_cog import WeatherCog
@@ -16,8 +24,8 @@ from cogs.meme_cog import MemeCog
 from cogs.stats_cog import StatsCog
 from cogs.management_cog import ManagementCog
 
+
 __all__ = [
-    "AdminCog", "FunCog", "PFMCog", "PUBGCog", "RedditCog", "SoundCog",
-    "UserCog", "WoWProgressCog", "TestingCog", "FryingCog", "WeatherCog",
-    "War3Cog", "ImageCog", "MemeCog", "StatsCog", "ManagementCog"
+    k for k, v in dict(locals()).items()
+    if k.endswith("Cog") and issubclass(v, commands.Cog)
 ]
