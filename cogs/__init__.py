bbd548e3a90df6410a999000734e5f54ebdcd321
PederHA <peder.andresen@gmail.com>
2019-04-03 17:20:12 +0200

    Add private cogs try-except-pass import.
    This allows me to add private cogs without maintaining 2 different __init__.py files

--- cogs/__init__.py
+++ cogs/__init__.py
@@ -9,7 +9,6 @@ from discord.ext import commands
 
 from cogs.admin_cog import AdminCog
 from cogs.fun_cog import FunCog
-from cogs.pfm_memes_cog import PFMCog
 from cogs.pubg_cog import PUBGCog
 from cogs.reddit_cog import RedditCog
 from cogs.user_cog import UserCog
@@ -17,13 +16,18 @@ from cogs.wowprogress_cog import WoWProgressCog
 from cogs.testing_cog import TestingCog
 from cogs.frying_cog import FryingCog
 from cogs.weather_cog import WeatherCog
-from cogs.war3_cog import War3Cog
 from cogs.image_cog import ImageCog
 from cogs.sound_cog import SoundCog
 from cogs.meme_cog import MemeCog
 from cogs.stats_cog import StatsCog
 from cogs.management_cog import ManagementCog
 
+# Private cogs. Delete this if you cloned the repo
+try:
+    from cogs.war3_cog import War3Cog
+    from cogs.pfm_cog import PFMCog
+except:
+    pass
 
 COGS = [
     v for k, v in dict(locals()).items()
