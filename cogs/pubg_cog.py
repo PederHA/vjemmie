153430489d8b32f80cca725e1f8da38fb4637b25
PederHA <peder.andresen@gmail.com>
2019-04-03 21:28:10 +0200

    Who still plays PUBG? Anyway, this isn't shit anymore

--- cogs/pubg_cog.py
+++ cogs/pubg_cog.py
@@ -12,112 +12,75 @@ class PUBGCog(BaseCog):
     """PUBG Bot Commands
     """
     
-    #@commands.command(
-    #    name="drop", aliases=["roulette", "plane"], description="u fucking wot"
-    #)
-    #async def drop(self, ctx: commands.Context, map_:str=None, opt: str=None):
-    #    """
-    #    ``!drop``command that chooses random drop location on a given PUBG map.
-    #    ----
-    #    
-    #    Args: tuple containing user arguments provided after typing !drop.
-    #    """
-#
-    #    MIRAMAR_LOCATIONS_ALL = [
-    #        "El Pozo",
-    #        "Pecado",
-    #        "San Martín",
-    #        "Hacienda del Patrón",
-    #        "Campo Militar",
-    #        "Los Leones",
-    #        "Monte Nuevo",
-    #        "El Azahar",
-    #        "Cruz del Valle",
-    #        "Tierra Bronca",
-    #        "Torre Ahumada",
-    #        "Impala",
-    #        "La Cobrería",
-    #    ]
-#
-    #    MIRAMAR_LOCATIONS_HOT = MIRAMAR_LOCATIONS_ALL[:6]
-#
-    #    ERANGEL_LOCATIONS_ALL = [
-    #        "South George",
-    #        "North George",
-    #        "Yasnaya",
-    #        "School",
-    #        "Pochinki",
-    #        "Mylta",
-    #        "Mylta Power",
-    #        "Military Base",
-    #        "Novorepnoye",
-    #        "Lipovka",
-    #        "Prison",
-    #        "Shelter",
-    #        "Primorsk",
-    #        "Gatka",
-    #        "Zharki",
-    #    ]
-#
-    #    ERANGEL_LOCATIONS_HOT = ERANGEL_LOCATIONS_ALL[:9]
-#
-    #    VALID_MAPS = ["erangel", "miramar", "sanhok [NYI!]"]
-    #    HOT = ["hot"]
-    #    invalid_args_msg = 'Invalid arguments. Syntax: "!drop <map> (hot)"'
-    #    invalid_map = False
-    #    no_map = False
-    #    pubgmap = map_.lower()
-    #    
-    #    if pubgmap not in VALID_MAPS:
-    #        await ctx.send("Invalid")
-    #        raise Exception
-#
-    #    # Only valid_args if an argument is given.
-    #    if map_ is not None:
-#
-    #        
-    #        if opt.lower() == "hot":
-    #            hot = True
-    #        else:
-    #            hot = False
-#
-    #        # Check if arg is a valid pubg map
-    #        if pubgmap in VALID_MAPS:
-    #            if pubgmap == VALID_MAPS[1]
-    #                if hot:
-    #                    droplocation = random.choice(MIRAMAR_LOCATIONS_HOT)
-    #                else:
-    #                    droplocation = random.choice(MIRAMAR_LOCATIONS_ALL)
-    #            elif pubgmap == VALID_MAPS[0]
-    #                if hot:
-    #                    droplocation = random.choice(ERANGEL_LOCATIONS_HOT)
-    #                else:
-    #                    droplocation = random.choice(ERANGEL_LOCATIONS_ALL)
-#
-    #            await ctx.send("Drop location: " + droplocation)
-#
-    #        else:
-    #            invalid_map = True
-#
-    #    else:
-    #        no_map = True
-    #    
-    #    if no_map or invalid_map:
-    #        if no_map:
-    #            message = "No map specified. Type "
-    #        if invalid_map:
-    #            message = "Invalid map specified. Type "
-    #        
-    #        n = 0
-    #        for pubgmap in VALID_MAPS:
-    #            n += 1
-    #            message+= f"`!drop {pubgmap} (hot)`"
-    #            if n < len(VALID_MAPS):
-    #                message += " or "
-    #        else:
-    #            message += "."
-    #        await ctx.send(message)
+    @commands.command(
+        name="drop", aliases=["roulette", "plane"], description="u fucking wot"
+    )
+    async def drop(self, ctx: commands.Context, map_:str=None, hot: str=None):
+        """
+        ``!drop``command that chooses random drop location on a given PUBG map.
+        """
+        MAPS = {
+            "miramar": { 
+                "locations": 
+                    [
+                    "El Pozo",
+                    "Pecado",
+                    "San Martín",
+                    "Hacienda del Patrón",
+                    "Campo Militar",
+                    "Los Leones",
+                    "Monte Nuevo",
+                    "El Azahar",
+                    "Cruz del Valle",
+                    "Tierra Bronca",
+                    "Torre Ahumada",
+                    "Impala",
+                    "La Cobrería",
+                    ],
+                "hot_idx": 6
+            },
+            "erangel": {
+                "locations": 
+                    [
+                    "South George",
+                    "North George",
+                    "Yasnaya",
+                    "School",
+                    "Pochinki",
+                    "Mylta",
+                    "Mylta Power",
+                    "Military Base",
+                    "Novorepnoye",
+                    "Lipovka",
+                    "Prison",
+                    "Shelter",
+                    "Primorsk",
+                    "Gatka",
+                    "Zharki",
+                    ],
+                "hot_idx": 9
+                }
+        }
 
+        # Get PUBG map
+        map_ = map_.lower()
+        pubgmap = MAPS.get(map_)
+
+        # Raise exception if map cannot be found
+        if not pubgmap:
+            raise discord.DiscordException("Invalid map!")
+
+        # Get list of locations for selected map
+        locations = pubgmap.get("locations")
+        
+        # Determine drop location selection logic
+        if hot:
+            hot_idx = pubgmap.get("hot_idx")
+            location = random.choice(locations[:hot_idx])
+        else:
+            location = random.choice(locations)
+
+        await ctx.send(location)
 
     # Start of Crate command
     @commands.command(
