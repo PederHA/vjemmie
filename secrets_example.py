6f9abdf692faed127ff50ccb50b66a17bc5c35ac
PederHA <peder.andresen@gmail.com>
2019-04-03 17:17:26 +0200

    Add secrets.py example

---
+++ secrets_example.py
@@ -0,0 +1,11 @@
+# Rename this file to secrets.py
+
+# Discord
+BOT_TOKEN = "BOT-TOKEN"
+DEV_BOT_TOKEN = "OPTIONAL-SECONDARY-BOT-TOKEN"
+
+# Reddit
+REDDIT_ID = "REDDIT-ID"
+REDDIT_SECRET = "REDDIT-SECRET"
+REDDIT_USER_AGENT = "<Custom user agent goes here>"
+
