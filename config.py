51fc5ece10a4a9072470cbf451a94fdcc2fa48e0
PederHA <peder.andresen@gmail.com>
2019-04-06 12:20:24 +0200

    Add DOWNLOADS_ALLOWED option

--- config.py
+++ config.py
@@ -1,8 +1,10 @@
 from utils.ext_models import SoundSubdir
 
+
 #///////////////////
 # Database        /
 #/////////////////
+
 # Delete this if cloning
 GENERAL_DB_PATH = "pfm.db"
 PFM_MEMES_DB_PATH = "memes/pfm_memes.db"
@@ -12,32 +14,39 @@ INSERT_STUFF_HERE = None
 #///////////////////
 # Cogs            /
 #/////////////////
+
 # Add cogs to disable !help message for. Makes cogs "unlisted".
-DISABLE_HELP = ["Admin", "Base", "Cod", "Weather", "YouTube", "War3"]
+DISABLE_HELP = ["Admin", "Cod", "Weather", "YouTube", "War3", "Management"]
 
 
 #///////////////////
 # Downloads       /
 #/////////////////
+
 MAX_DL_SIZE = 25_000_000 # 25 MB
+DOWNLOADS_ALLOWED = True # Enables download commands such as !ytdl and !add_sound
+
 
 #///////////////////
-# Channels       /
+# Channels        /
 #/////////////////
-IMAGE_CHANNEL_ID = 549649397420392567 # Upload images
-LOG_CHANNEL_ID = 340921036201525248 # Log errors
-DOWNLOAD_CHANNEL_ID = 563312629045788692 # Log downloads
+
+# Replace all channel and user IDs
+IMAGE_CHANNEL_ID = 549649397420392567 # Image rehosting channel
+LOG_CHANNEL_ID = 340921036201525248 # Error logging channel
+DOWNLOAD_CHANNEL_ID = 563312629045788692 # Download logging channel
 AUTHOR_MENTION = "<@103890994440728576>" # Creates an @BOT_AUTHOR mention. Replace with own ID
 
 
 #///////////////////
 # Soundboard      /
 #/////////////////
+
 SOUND_DIR = "sounds" # Base sound directory
 SoundSubdir.SOUND_DIR = SOUND_DIR # Don't touch this.
 
 # Sound subdirectories
-#                      Folder  Aliases
+#                    Directory  Aliases
 YTDL_DIR = SoundSubdir("ytdl", ["youtube", "utube", "yt"])
 TTS_DIR = SoundSubdir("tts", ["text-to-speech", "texttospeech"])
 GENERAL_DIR = SoundSubdir("general", ["other", "normal", "uncategorized"])
