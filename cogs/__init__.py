9c8b3356bac23727f2f1af497046a90d284e9148
PederHA <peder.andresen@gmail.com>
2019-04-17 13:18:27 +0200

    Improve import of private cogs
    This approach is better, partly due to its increased readability, but also because all exceptions but ImportError are raised normally.
    
    In my opinion, contextlib.suppress() is more elegant for letting a single exception class pass silently while also improving readability.

--- cogs/__init__.py
+++ cogs/__init__.py
@@ -3,6 +3,7 @@ Add/remove bot cogs in this module.
 """
 
 from discord.ext import commands
+from contextlib import suppress
 
 from cogs.admin_cog import AdminCog
 from cogs.fun_cog import FunCog
@@ -21,12 +22,10 @@ from cogs.management_cog import ManagementCog
 from cogs.test_cog import TestCog
 
 # Private cogs. You can delete this if you cloned the repo
-try:
+with suppress(ImportError):
     from cogs.war3_cog import War3Cog
-    from cogs.pfm_cog import PFMCog
-except Exception as e:
-    print(e)
-    pass
+    from cogs.pfm_cog import PFMCog    
+
 
 # List of all cogs in the local namespace
 # This should be imported in the bot's main module
