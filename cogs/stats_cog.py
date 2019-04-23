1e2a412f77c75000d19d93870806931dd9780476
PederHA <peder.andresen@gmail.com>
2019-04-23 19:38:56 +0200

    Rename `!commits` to `!changelog`. Add days argument.
    Also moved start_time from cog to bot

--- cogs/stats_cog.py
+++ cogs/stats_cog.py
@@ -1,13 +1,14 @@
 from discord.ext import commands
 import discord
 from cogs.base_cog import BaseCog
-from datetime import datetime
+from datetime import datetime, timedelta
 from time import perf_counter, time
 import json
+from functools import partial
 
 from itertools import islice
 
-from github import Github
+from github import Github, GithubObject
 
 from ext.checks import owners_only
 from botsecrets import GITHUB_TOKEN
@@ -19,12 +20,13 @@ class StatsCog(BaseCog):
 
     def __init__(self, bot: commands.Bot) -> None:
         super().__init__(bot)
-        self.START_TIME = datetime.now()
+        self.bot.start_time = datetime.now()
         self.github = Github(GITHUB_TOKEN)
 
     @commands.command(name="uptime")
     async def uptime(self, ctx: commands.Context) -> None:
-        up = datetime.now() - self.START_TIME
+        """Bot uptime."""
+        up = datetime.now() - self.bot.start_time
         days = up.days
         hours, remainder = divmod(up.seconds, 3600)
         minutes, seconds = divmod(remainder, 60)
@@ -43,18 +45,12 @@ class StatsCog(BaseCog):
     @commands.command(name="get_players")
     @owners_only()
     async def get_players(self, ctx) -> None:
+        """ADMIN ONLY: Display active audio players."""
         sound_cog = self.bot.get_cog("SoundCog")
         players = sound_cog.players
-        #with open("out/audioplayers.json", "r") as f:
-        #    players = json.load(f)
-        
+      
         out = []
         for gid, player in players.items():
-            #embed_body = ( 
-            #        f"**Created at**: {player['created_at']}\n"
-            #        f"**Currently playing**: {player['current']}"
-            #        f"**Guild ID**: {gid}\n"
-            #        )
             embed_body = ( 
                     f"**Created at**: {player.created_at}\n"
                     f"**Currently playing**: {player.current.title if player.current else None}\n"
@@ -68,21 +64,39 @@ class StatsCog(BaseCog):
                 await self.send_embed_message(ctx, gname, o, footer=False)
         else:
             await ctx.send("No active audio players")
-
-    @commands.command(name="commits")
-    async def git_commits(self, ctx: commands.Context) -> None:
+    
+    @commands.command(name="changelog", aliases=["commits"])
+    @commands.cooldown(rate=1, per=10, type=commands.BucketType.guild)
+    async def changelog(self, ctx: commands.Context, days: int=0) -> str:
+        """Display git commmit log"""
+        # Limit number of days to get commits from
+        if days > 7:
+            raise ValueError("Number of days cannot exceed 7!")
+        
         # Get repo
         repo = self.github.get_repo("PederHA/vjemmie")
 
-        # Only retrieve 5 most recent commits 
-        commits = list(islice(repo.get_commits(), 5))
+        # Get commits
+        since = datetime.now() - timedelta(days=days) if days else GithubObject.NotSet
+        to_run = partial(repo.get_commits, since=since)
+        _c = await self.bot.loop.run_in_executor(None, to_run)
+        
+        if days:
+            commits = list(_c)
+        else:
+            # Get 5 most recent commits if no days argument
+            commits = list(islice(_c, 5))
         
         # Format commit hash + message
+        n = "\n"
         out_commits = "\n".join(
             [
-            f"[`{commit.sha[:7]}`]({commit.html_url}): {commit.commit.message}"
+            f"[`{commit.sha[:7]}`]({commit.html_url}): {commit.commit.message.split(n, 1)[0]}"
             for commit in commits
             ]
         )
 
         await self.send_embed_message(ctx, "Commits", out_commits, color=0x24292e)
+        
+        # Return commit history
+        return out_commits
