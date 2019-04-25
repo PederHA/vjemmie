4470955f31b97ed92e3e145f723f6ab0b203ae45
PederHA <peder.andresen@gmail.com>
2019-04-25 12:30:50 +0200

    Add dynamic author mention f-string in config

--- config.py
+++ config.py
@@ -24,6 +24,12 @@ INSERT_STUFF_HERE = None
 MAX_DL_SIZE = 25_000_000 # 25 MB
 DOWNLOADS_ALLOWED = True # Enables download commands such as !ytdl and !add_sound
 
+#///////////////////
+# Users           /
+#/////////////////
+
+OWNER_ID = 103890994440728576 # Replace with own User ID
+AUTHOR_MENTION = f"<@{OWNER_ID}>" # Creates an @BOT_AUTHOR mention.
 
 #///////////////////
 # Channels        /
@@ -33,7 +39,6 @@ DOWNLOADS_ALLOWED = True # Enables download commands such as !ytdl and !add_soun
 IMAGE_CHANNEL_ID = 549649397420392567 # Image rehosting channel
 LOG_CHANNEL_ID = 340921036201525248 # Error logging channel
 DOWNLOAD_CHANNEL_ID = 563312629045788692 # Download logging channel
-AUTHOR_MENTION = "<@103890994440728576>" # Creates an @BOT_AUTHOR mention. Replace with own ID
 GUILD_HISTORY_CHANNEL = 565674480660119592 # Joining/leaving guilds
 
 #///////////////////
@@ -54,9 +59,3 @@ DOWNLOADS_DIR = SoundSubdir("downloads", ["dl", "downloaded", "download"])
 # Dynamic list of SoundSubdir instances. Import this one!
 SOUND_SUB_DIRS = [v for v in dict(locals()).values() if isinstance(v, SoundSubdir)]
 
-#///////////////////
-# Users           /
-#/////////////////
-
-# List of user IDs with highest privileges for bot commands
-OWNERS = [103890994440728576]
