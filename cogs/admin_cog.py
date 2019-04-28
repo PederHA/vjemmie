efdbca2de497f44ac3353180558569af44ebcf3b
PederHA <peder.andresen@gmail.com>
2019-04-28 13:08:15 +0200

    smh

--- cogs/admin_cog.py
+++ cogs/admin_cog.py
@@ -21,7 +21,7 @@ class AdminCog(BaseCog):
         activity_name = "!help" if not activity_name else activity_name
         activity = discord.Game(activity_name)
         await self.bot.change_presence(activity=activity)
-        print("Client logged in")
+        print("Bot logged in")
     
     @commands.Cog.listener()
     async def on_guild_join(self, guild: discord.Guild) -> None:
