9df401d58d93db5a4888a5813d678de3470d1bed
PederHA <peder.andresen@gmail.com>
2019-05-02 15:20:51 +0200

    Add experimental param to `!goodmorning`

--- cogs/meme_cog.py
+++ cogs/meme_cog.py
@@ -13,6 +13,7 @@ class MemeCog(BaseCog):
     
     def __init__(self, bot) -> None:
         super().__init__(bot)
+        self.experimental = False
         self.wordlist = []
     
     @commands.command(name="goodshit")
@@ -31,11 +32,18 @@ class MemeCog(BaseCog):
         await self.read_send_file(ctx, "memes/txt/madara.txt")
 
     @commands.command(name="goodmorning")
-    async def goodmorning(self, ctx: commands.Context) -> None:
-        if not self.wordlist:
-            with open("memes/txt/6of12.txt", "r") as f:
+    async def goodmorning(self, ctx: commands.Context, experimental: bool=False) -> None:
+        if not self.wordlist or experimental != self.experimental:
+            if experimental:
+                fp = "memes/txt/6of12.txt"
+                self.experimental = True
+            else:
+                fp = "memes/txt/nationalities.txt"
+                self.experimental = False
+
+            with open(fp, "r") as f:
                 self.wordlist = f.read().splitlines()
-        
+            
         word = random.choice(self.wordlist)
         
-        await ctx.send(f"Good morning to everyone apart from {word}s")
+        await ctx.send(f"Good morning to everyone apart from the {word}")
