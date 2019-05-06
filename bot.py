e1b8433a7e9fe33a28587ece71da23da08bb3141
PederHA <peder.andresen@gmail.com>
2019-05-06 20:30:11 +0200

    Add uvloop import for Linux/Mac systems
    Uvloop is not added the Pipfile, and is not something I have extensively tested.

--- bot.py
+++ bot.py
@@ -1,10 +1,15 @@
 import sys
+from contextlib import suppress
+
+with suppress(ImportError):
+    import uvloop
+    uvloop.install()
 
 from discord.ext.commands import Bot
 
 from cogs import COGS
 from botsecrets import BOT_TOKEN, DEV_BOT_TOKEN
-
+    
 # Bot setup
 bot = Bot(command_prefix="!", description="Meme bot", pm_help=False)
 
