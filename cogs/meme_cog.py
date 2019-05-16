22eeda992a2f0bf96043d1c57bc84f343ceb74d2
PederHA <peder.andresen@gmail.com>
2019-05-16 12:02:38 +0200

    Add r/emojipasta markov chain post generator

--- cogs/meme_cog.py
+++ cogs/meme_cog.py
@@ -2,7 +2,7 @@ import random
 
 import discord
 from discord.ext import commands
-
+import markovify
 from cogs.base_cog import BaseCog
 
 
@@ -15,6 +15,7 @@ class MemeCog(BaseCog):
         super().__init__(bot)
         self.experimental = False
         self.wordlist = []
+        self.models = {}
     
     @commands.command(name="goodshit")
     async def goodshit(self, ctx: commands.Context) -> None:
@@ -47,3 +48,21 @@ class MemeCog(BaseCog):
         word = random.choice(self.wordlist)
         
         await ctx.send(f"Good morning to everyone apart from the {word}")
+
+    @commands.command(name="emojipastam")
+    async def emojipasta_markovchain(self, ctx: commands.Context) -> None:
+        subreddit = "emojipasta"
+        
+        if subreddit not in self.models:
+            reddit_cog = self.bot.get_cog("RedditCog")
+            
+            posts = await reddit_cog.get_from_reddit(ctx, subreddit, is_text=True, rtn_posts=True)
+            text = "\n".join([post.selftext for post in posts if post.selftext])
+            model = markovify.NewlineText(text)
+            
+            self.models[subreddit] = model
+        else:
+            model = self.models.get(subreddit)
+        
+        await ctx.send(model.make_sentence(tries=300))
+        
