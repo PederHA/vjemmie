69976ed491f0cdf80029f293811068345aac45d1
PederHA <peder.andresen@gmail.com>
2019-05-21 12:35:24 +0200

    Update bot main parameters

--- bot.py
+++ bot.py
@@ -10,16 +10,16 @@ from discord.ext.commands import Bot
 from cogs import COGS
 from botsecrets import BOT_TOKEN
 
-def main(token, *dev_cogs) -> None:       
+def main(token, cogs) -> None:       
     # Bot setup
     bot = Bot(command_prefix="!", description="Meme bot", pm_help=False)
 
     # Add cogs
-    for cog in COGS + list(dev_cogs):
+    for cog in cogs:
             bot.add_cog(cog(bot=bot))
 
     # Run bot
     bot.run(token)
 
 if __name__ == "__main__":
-    main(BOT_TOKEN)
+    main(BOT_TOKEN, COGS)
--- testbot.py
+++ testbot.py
@@ -1,8 +1,10 @@
 from bot import main
 from botsecrets import DEV_BOT_TOKEN
 from tests.test_cog import TestCog
+from cogs import COGS
 
 
 if __name__ == "__main__":
     print("Dev mode active")
-    main(DEV_BOT_TOKEN, TestCog)
+    test_cogs = [TestCog]
+    main(DEV_BOT_TOKEN, COGS + test_cogs)
