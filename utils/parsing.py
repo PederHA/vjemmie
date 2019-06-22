412c58fefa6b7fd4bd30f0d3a73647f804bb3bb1
PederHA <peder.andresen@gmail.com>
2019-06-22 21:17:43 +0200

    Replace period with underscore in valid command chars

--- utils/parsing.py
+++ utils/parsing.py
@@ -1,4 +1,4 @@
 import re
 
-def is_valid_command_name(command: str, search=re.compile(r'[^a-z0-9.]').search) -> bool:
+def is_valid_command_name(command: str, search=re.compile(r'[^a-z0-9_]').search) -> bool:
     return not bool(search(command))
