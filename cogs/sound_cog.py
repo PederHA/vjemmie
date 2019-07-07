208db5a3fbe923f6fc0a2caac9ac01f0bda705a9
PederHA <peder.andresen@gmail.com>
2019-07-07 13:05:06 +0200

    Increase FFMPEG reconnect delay max value

--- cogs/sound_cog.py
+++ cogs/sound_cog.py
@@ -55,7 +55,7 @@ ytdlopts = {
 }
 
 ffmpegopts = {
-    'before_options': "-nostdin -reconnect 1 -reconnect_at_eof 1 -reconnect_streamed 1 -reconnect_delay_max 5",
+    'before_options': "-nostdin -reconnect 1 -reconnect_at_eof 1 -reconnect_streamed 1 -reconnect_delay_max 60",
     'options': '-vn'
 }
 
