6846a53375ded8106bc2094d3c148d97a56977f8
PederHA <peder.andresen@gmail.com>
2019-09-03 13:31:43 +0200

    Add public general purpose mimetype function

--- utils/filetypes.py
+++ utils/filetypes.py
@@ -7,14 +7,23 @@ T = Union[BinaryIO, bytes]
 def _get_file_mimetype(file_: T, bufsize: int) -> str:
     if hasattr(file_, "read"):
         b = file_.read(bufsize)
+        file_.seek(0) # is this necessary?
     else:
         b = file_
     return magic.from_buffer(b, mime=True)
 
+
 def check_file_audio(file_: T, bufsize: int=1024) -> bool:
     return _get_file_mimetype(file_, bufsize).startswith("audio/")
+
 
 def check_file_video(file_: T, bufsize: int=1024) -> bool:
     return _get_file_mimetype(file_, bufsize).startswith("video/")
 
 
+def check_file_image(file_: T, bufsize: int=1024) -> bool:
+    return _get_file_mimetype(file_, bufsize).startswith("image/")
+
+
+def get_file_mimetype(file_: T, bufsize: int=1024) -> str:
+    return _get_file_mimetype(file_, bufsize)
