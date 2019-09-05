3904ba855f691779550b5063a044a5ca745196e6
PederHA <peder.andresen@gmail.com>
2019-09-05 20:04:31 +0200

    Add BUFSIZE var

--- utils/filetypes.py
+++ utils/filetypes.py
@@ -4,6 +4,8 @@ import magic
 
 T = Union[BinaryIO, bytes]
 
+BUFSIZE = 1024
+
 def _get_file_mimetype(file_: T, bufsize: int) -> str:
     if hasattr(file_, "read"):
         b = file_.read(bufsize)
@@ -13,17 +15,17 @@ def _get_file_mimetype(file_: T, bufsize: int) -> str:
     return magic.from_buffer(b, mime=True)
 
 
-def check_file_audio(file_: T, bufsize: int=1024) -> bool:
+def check_file_audio(file_: T, bufsize: int=BUFSIZE) -> bool:
     return _get_file_mimetype(file_, bufsize).startswith("audio/")
 
 
-def check_file_video(file_: T, bufsize: int=1024) -> bool:
+def check_file_video(file_: T, bufsize: int=BUFSIZE) -> bool:
     return _get_file_mimetype(file_, bufsize).startswith("video/")
 
 
-def check_file_image(file_: T, bufsize: int=1024) -> bool:
+def check_file_image(file_: T, bufsize: int=BUFSIZE) -> bool:
     return _get_file_mimetype(file_, bufsize).startswith("image/")
 
 
-def get_file_mimetype(file_: T, bufsize: int=1024) -> str:
+def get_file_mimetype(file_: T, bufsize: int=BUFSIZE) -> str:
     return _get_file_mimetype(file_, bufsize)
