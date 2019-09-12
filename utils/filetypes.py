ba63ee34c9f1f54ac85ec388e4679e8e9f1583f0
PederHA <peder.andresen@gmail.com>
2019-09-12 19:40:26 +0200

    Remove underscore from param name

--- utils/filetypes.py
+++ utils/filetypes.py
@@ -6,29 +6,29 @@ T = Union[BinaryIO, bytes]
 
 BUFSIZE = 1024
 
-def _get_file_mimetype(file_: T, bufsize: int) -> str:
-    if hasattr(file_, "read"):
-        pos = file_.tell()
+def _get_file_mimetype(file: T, bufsize: int) -> str:
+    if hasattr(file, "read"):
+        pos = file.tell()
         if pos != 0:
-            file_.seek(0)
-        b = file_.read(bufsize)
-        file_.seek(pos)
+            file.seek(0)
+        b = file.read(bufsize)
+        file.seek(pos)
     else:
-        b = file_
+        b = file
     return magic.from_buffer(b, mime=True)
 
 
-def check_file_audio(file_: T, bufsize: int=BUFSIZE) -> bool:
-    return _get_file_mimetype(file_, bufsize).startswith("audio/")
+def check_file_audio(file: T, bufsize: int=BUFSIZE) -> bool:
+    return _get_file_mimetype(file, bufsize).startswith("audio/")
 
 
-def check_file_video(file_: T, bufsize: int=BUFSIZE) -> bool:
-    return _get_file_mimetype(file_, bufsize).startswith("video/")
+def check_file_video(file: T, bufsize: int=BUFSIZE) -> bool:
+    return _get_file_mimetype(file, bufsize).startswith("video/")
 
 
-def check_file_image(file_: T, bufsize: int=BUFSIZE) -> bool:
-    return _get_file_mimetype(file_, bufsize).startswith("image/")
+def check_file_image(file: T, bufsize: int=BUFSIZE) -> bool:
+    return _get_file_mimetype(file, bufsize).startswith("image/")
 
 
-def get_file_mimetype(file_: T, bufsize: int=BUFSIZE) -> str:
-    return _get_file_mimetype(file_, bufsize)
+def get_file_mimetype(file: T, bufsize: int=BUFSIZE) -> str:
+    return _get_file_mimetype(file, bufsize)
