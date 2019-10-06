6a4e93ce6f6aa0e62c49ae71ba4d455451bc1864
PederHA <peder.andresen@gmail.com>
2019-10-07 00:32:13 +0200

    Improve docstring

--- utils/users.py
+++ utils/users.py
@@ -4,5 +4,5 @@ import discord
 from discord.ext.commands import Bot
 
 def get_users_from_ids(bot: Bot, users: Iterable[int]) -> List[discord.User]:
-    """Parses an iterable of user IDs and returns list of `discord.User` objects."""
+    """Takes an iterable of user IDs and returns list of `discord.User` objects."""
     return list(filter(None.__ne__, [bot.get_user(user) for user in users]))
