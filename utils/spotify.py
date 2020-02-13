35ce506f9d54d203ef3e16ee7096f9c854eddacc
PederHA <peder.andresen@gmail.com>
2020-02-13 09:37:25 +0100

    Split up spotify song info func

--- utils/spotify.py
+++ utils/spotify.py
@@ -6,11 +6,24 @@ from spotipy.oauth2 import SpotifyClientCredentials
 from botsecrets import SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET
 from utils.exceptions import CommandError
 
+
 client_credentials_manager = SpotifyClientCredentials(client_id=SPOTIFY_CLIENT_ID, client_secret=SPOTIFY_CLIENT_SECRET)
 spotify = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
+
 def get_spotify_song_info(arg: str) -> Tuple[str, str, str]:
     """Fetches artist, song and album from a Spotify URL or URI."""
+    track_id = get_spotify_track_id(arg)
+
+    track = spotify.track(track_id)
+    artists = ", ".join([artist["name"] for artist in track["artists"]])
+    song = track["name"]
+    album = track["album"]["name"]
+
+    return artists, song, album
+
+
+def get_spotify_track_id(arg: str) -> str:
     if arg.startswith("spotify:track:"):
         track_id = arg.split("spotify:track:")[1]
     
@@ -26,9 +39,7 @@ def get_spotify_song_info(arg: str) -> Tuple[str, str, str]:
     else:
         raise CommandError("Unrecognized spotify URI/URL")
 
-    track = spotify.track(track_id)
-    artists = ", ".join([artist["name"] for artist in track["artists"]])
-    song = track["name"]
-    album = track["album"]["name"]
+    return track_id
+
 
     return artists, song, album
