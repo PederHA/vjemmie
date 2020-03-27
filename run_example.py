974eaa3d31e7403ba03e14f3e65d97f792e6ac79
PederHA <peder.andresen@gmail.com>
2020-03-27 17:48:05 +0100

    Add try/except

--- run_example.py
+++ run_example.py
@@ -1,10 +1,15 @@
+from contextlib import suppress
 from typing import List, Optional
 
-import vjemmie
 from discord.ext.commands import Cog
+import vjemmie
 
-from mycogs import cogs # Define custom cogs in your own module
 import botsecrets # Pass entire botsecrets module to vjemmie.run
+
+try:
+    from mycogs import cogs # Define custom cogs in your own module
+except ImportError:
+    cogs = []
 
 
 def main(secrets, cogs: Optional[List[Cog]]=None) -> None:       
