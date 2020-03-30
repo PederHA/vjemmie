4889a5c3528faddd7776ce53b1e12ac9cf3fe4fd
PederHA <peder.andresen@gmail.com>
2020-03-30 16:38:02 +0200

    Add optional pfmcog import

--- vjemmie/cogs/__init__.py
+++ vjemmie/cogs/__init__.py
@@ -1,6 +1,8 @@
 """
 Add/remove bot cogs in this module.
 """
+from contextlib import suppress
+
 from discord.ext import commands
 
 from .admin_cog import AdminCog
@@ -20,6 +22,8 @@ from .user_cog import UserCog
 from .weather_cog import WeatherCog
 from .wowprogress_cog import WoWProgressCog
 
+with suppress(ImportError):
+    from .pfm_cog import PFMCog
 
 COGS = [
         v for k, v in dict(locals()).items()
