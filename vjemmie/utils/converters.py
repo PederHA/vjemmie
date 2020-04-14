ca0b7d19eb7397d07888e38ef94e0e1f6b4ce3c9
PederHA <peder.andresen@gmail.com>
2020-04-14 16:11:38 +0200

    Add case insensitive MemberConverter

--- vjemmie/utils/converters.py
+++ vjemmie/utils/converters.py
@@ -59,6 +59,59 @@ class MemberOrURLConverter(IDConverter):
 
         return result
 
+
+class NonCaseSensMemberConverter(IDConverter):
+    """Converts to a :class:`Member` if possible.
+    If object passed in is a string that looks like an HTTP/HTTPS
+    URL, returns string.
+
+    All lookups are via the local guild. If in a DM context, then the lookup
+    is done by the global cache.
+
+    The lookup strategy is as follows (in order):
+
+    1. Lookup by ID.
+    2. Lookup by mention.
+    3. Lookup by name#discrim
+    4. Lookup by name (Case insensitive)
+    5. Lookup by nickname (Case insensitive)
+    """
+
+    async def convert(self, ctx, argument):
+        bot = ctx.bot
+        match = self._get_id_match(argument) or re.match(r'<@!?([0-9]+)>$', argument)
+        guild = ctx.guild
+        result = None
+        argument = argument.lower()
+
+        def get_from_guild(guild: discord.Guild, argument: Union[str, int]) -> Optional[discord.Member]:
+            for member in guild.members:
+                if member.name.lower() == argument or member.nick.lower() == argument:
+                    return member
+    
+        # This is a really ugly re-purposing of the original MemberConverter
+        if match is None:
+            if guild:
+                result = get_from_guild(guild, argument)
+            else:
+                for guild in bot.guilds:
+                    result = get_from_guild(guild, argument)
+                    if result:
+                        break
+        else:
+            user_id = int(match.group(1))
+            if guild:
+                result = guild.get_member(user_id)
+            else:
+                result = _get_from_guilds(bot, 'get_member', user_id)
+
+
+        if result is None:
+            raise BadArgument(f"No member named '{argument}''")
+
+        return result
+
+
 class UserOrMeConverter(IDConverter):
     """Converts to a :class:`User`.
 
