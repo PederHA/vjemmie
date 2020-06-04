69a0ef8ec0f5c5fdac0e09c9c6f0fb3b655574ad
PederHA <peder.andresen@gmail.com>
2020-06-04 11:40:15 +0200

    Add docstring and proper type annotation

--- vjemmie/utils/parsing.py
+++ vjemmie/utils/parsing.py
@@ -1,11 +1,15 @@
 import re
-
+from typing import Tuple
 
 def is_valid_command_name(command: str, search=re.compile(r'[^a-z0-9_]').search) -> bool:
     return not bool(search(command))
 
 
-def split_text_numbers(text: str):
+def split_text_numbers(text: str) -> Tuple[str, str]:
+    """
+    >>> split_text_numbers("foo123")
+    ('foo', '123')
+    """
     head = text.rstrip("0123456789")
     tail = text[len(head):]
     return head, tail
