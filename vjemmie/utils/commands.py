a1a679b9fe7bb01c21ba67a4acc8f3e34325b784
PederHA <peder.andresen@gmail.com>
2020-06-17 18:11:41 +0200

    Add 'checks' param to add_command()

--- vjemmie/utils/commands.py
+++ vjemmie/utils/commands.py
@@ -12,6 +12,7 @@ def add_command(cog: commands.Cog,
                 help: Optional[str]=None,
                 hidden: bool=False,
                 group: Optional[commands.Group]=None,
+                checks: Optional[List[Callable]] = None,
                 **kwargs
                ) -> None:
     _cmd = coroutine(partial(coro, **kwargs)) # Create partial coro from passed in coro
@@ -24,6 +25,8 @@ def add_command(cog: commands.Cog,
     
     if not aliases:
         aliases = []
+    if not checks:
+        checks = []
 
     if not help:
         help = ""
@@ -32,4 +35,5 @@ def add_command(cog: commands.Cog,
     cmd = cmd_func(name=name, aliases=aliases, cog=cog, help=help)(_cmd)
     cmd.cog = cog
     cmd.hidden = hidden
+    cmd.checks.extend(checks)
     cog.bot.add_command(cmd)
