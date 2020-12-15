33f9d799ccc345cad9ddd14b4c948fe9b269dc85
Peder Hovdan Andresen <pedeha@stud.ntnu.no>
2020-12-15 11:52:49 +0100

    Use actual params for blocking_dump(), not *args

--- vjemmie/utils/json.py
+++ vjemmie/utils/json.py
@@ -19,9 +19,10 @@ def dump_json_blocking(fp: str, obj: Any, default: Any=None) -> None:
     with open(fp, "w") as f:
         f.write(d)
 
+
 if in_wsl():
     # AIOFile doesn't seem to work on Microsoft's 4.4.0-18362 Linux kernel
     # Warning received on startup: <frozen importlib._bootstrap>:219: RuntimeWarning: Linux supports fsync/fdsync with io_submit since 4.18 but current kernel 4.4.0-18362-Microsoft doesn't support it. Related calls will have no effect.
-    async def blocking_dump(*args):
-        return dump_json_blocking(*args)
+    async def blocking_dump(fp: str, obj: Any, default: Any=None) -> None:
+        return dump_json_blocking(fp, obj, default)
     dump_json = blocking_dump
